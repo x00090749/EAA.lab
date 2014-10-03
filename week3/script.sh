00535503c49dbc94845c631e768138a6bcbd3a59
x00090749 <x00090749@ittd.ie>
2014-10-03 15:45:14 +0100

    latest

--- week3/script.sh
+++ week3/script.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# Title: script.sh
+# Date : 3 Oct 2014
+# Author : jzburns
+# Version :1.0
+# Description : counts the number of CPUs in a VM
+# Options : No options
 
-#This is a script
-#this is a comment
 
-grep processor /proc/cpuinfo | wc -l
+cpucnt=$( grep processor /proc/cpuinfo | wc -l )
+
+printf "The Number of CPUs is: %s\n" $cpucnt
